1722e18ed3220615e269bcd4137142164e8b3277
issei-yoshi <iyoshi.engineer@gmail.com>
2023-05-06 13:35:04 +0900

    add: ルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # 名前空間を付与することでコントローラをグルーピングする
+  namespace :api do
+    # バージョンをつけることでAPIを更新する場合にスイッチングしやすくするため
+    namespace :v1 do
+      resources :restaurants do
+        resources :foods, only: %i[index]
+      end
+      resources :line_foods, only: %i[index]
+      put 'line_foods/replace', to: 'line_foods#replace'
+      resources :orders, only: %i[create]
+    end
+  end
 end
